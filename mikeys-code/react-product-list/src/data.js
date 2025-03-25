01687bafe32980e2801139428e6b39dedf4c1d6c
mikey <mikey@hey.com>
2025-03-25 16:35:15 -0700

    Refactor to use destructuring, helper functions, etc.

--- mikeys-code/react-product-list/src/data.js
+++ mikeys-code/react-product-list/src/data.js
@@ -1,16 +1,26 @@
 import data from './data.json';
 
 // base challenges:
-export const categories = data.map((x) => x.category);
+const categoriesList = data.map((x) => x.category);
 
-export const uniqueCategories = [
-  ...new Set(data.map((x) => x.category)),
-];
+const parsePrice = (price) => parseFloat(price.replace(/\$/g, ''));
+
+export const categories = [...categoriesList];
+
+export const uniqueCategories = [...new Set(categoriesList)];
 
 export const categoryCount = data.reduce((acc, product) => {
   acc[product.category] = (acc[product.category] || 0) + 1;
   return acc;
 }, {});
+/* 
+{
+  Movies: 6,
+  Grocery: 4,
+  Baby: 7, ...
+  ...
+}
+*/
 
 export const categoryCounts = Object.entries(categoryCount).map(
   ([name, count]) => ({
@@ -18,32 +28,32 @@ export const categoryCounts = Object.entries(categoryCount).map(
     count,
   })
 );
+/*
+[
+   {name: "Movies", count: 6}, 
+   {name: "Grocery", count: 4}, 
+   {name: "Baby", count: 7},
+   ...
+ ]
+*/
 
 // stretch challenges:
-export const priceList = data.map((x) => ({
-  name: x.name,
-  price: x.price,
+export const priceList = data.map(({ name, price }) => ({
+  name,
+  price,
 }));
 
-export const expensiveProducts = data.filter((x) => {
-  // Remove the '$' and convert to a number
-  const price = parseFloat(x.price.replace('$', ''));
-  return price > 50;
-});
+export const expensiveProducts = data.filter(
+  ({ price }) => parsePrice(price) > 50
+);
 
-export const totalInventoryValue = data.reduce((acc, x) => {
-  const price = parseFloat(x.price.replace('$', ''));
-  return acc + price * x.units;
-}, 0);
+export const totalInventoryValue = data.reduce(
+  (acc, x) => acc + parsePrice(x.price) * x.units,
+  0
+);
 
 export const categoryTotalValue = data.reduce((acc, product) => {
-  const price = parseFloat(product.price.replace('$', ''));
-
-  if (!acc[product.category]) {
-    acc[product.category] = 0;
-  }
-
-  acc[product.category] += price;
-
+  const price = parsePrice(product.price);
+  acc[product.category] = (acc[product.category] || 0) + price;
   return acc;
 }, {});
